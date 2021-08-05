0a51b1395cddf572522fc7f8c20e7768e2c26886
Santiago <santiagocernadas29@gmail.com>
2021-08-05 18:11:44 -0300

    Version 0.04: Plantilla para LetraActivity que cuenta con un titulo, 3 palabras de ejemplos y 3 imagenes.

--- app/src/main/java/com/example/proyectodante/LetraActivity.java
+++ app/src/main/java/com/example/proyectodante/LetraActivity.java
@@ -1,10 +1,10 @@
 package com.example.proyectodante;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LetraActivity extends AppCompatActivity {
 
@@ -16,7 +16,34 @@ public class LetraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letra);
         String letraSeleccionada = getIntent().getStringExtra("letra");
+
+
         titulo = findViewById(R.id.txt_letra_titulo);
+        tv1 = findViewById(R.id.txt_letra_ej1);
+        tv2 = findViewById(R.id.txt_letra_ej2);
+        tv3 = findViewById(R.id.txt_letra_ej3);
+        img1 = findViewById(R.id.img_ej1);
+        img2 = findViewById(R.id.img_ej2);
+        img3 = findViewById(R.id.img_ej3);
+
+
         titulo.setText("Letra " + letraSeleccionada);
+        generarActivity(letraSeleccionada);
+    }
+
+    public void generarActivity(String letra){
+        switch (letra){
+            case "A":
+                tv1.setText("Arbol");
+                img1.setImageResource(R.drawable.aejuno);
+                tv2.setText("Abeja");
+                img2.setImageResource(R.drawable.aejdos);
+                tv3.setText("Avion");
+                img3.setImageResource(R.drawable.aejtres);
+                break;
+            default:
+                tv1.setText("Error");
+                break;
+        }
     }
 }
