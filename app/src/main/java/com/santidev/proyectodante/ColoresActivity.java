efa7326066f72066a4a3234365f9873df9360baf
santiagoCernadas <santiagocernadas29@gmail.com>
2025-03-26 23:41:23 -0300

    Se añade la pantalla color

--- app/src/main/java/com/santidev/proyectodante/ColoresActivity.java
+++ app/src/main/java/com/santidev/proyectodante/ColoresActivity.java
@@ -5,7 +5,9 @@ import static com.santidev.proyectodante.MainActivity.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColoresActivity extends AppCompatActivity {
 
@@ -15,6 +17,23 @@ public class ColoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colores);
+    }
+
+    public void teclaPresionada(View view){
+        switch (view.getId()){
+            case R.id.button_amarillo:
+                startLetraActivity("amarillo");
+                break;
+            default:
+                startLetraActivity("ERROR");
+                break;
+        }
+    }
+
+    public void startLetraActivity(String color){
+        Intent i = new Intent(this,LetraActivity.class);
+        i.putExtra("color",color);
+        startActivity(i);
     }
 
     public void onPause() {
